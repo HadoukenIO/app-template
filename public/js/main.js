04b7a73175d9ae2d9945df2847a1b3cbe854c5a8
Ricardo De Pena <ricardo@isnull.co>
2017-06-06 23:57:05 -0400

    fixed a few issue and added a README.md file

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,17 @@
 //event listeners.
-document.addEventListener('DOMContentLoaded', function() {
-    const ofVersion = document.querySelector('#of-version');
+document.addEventListener('DOMContentLoaded', () => {
+
     if (typeof fin != 'undefined') {
-        fin.desktop.System.getVersion(function(version) {
-            ofVersion.innerText = version;
-        });
+	fin.desktop.main(onMain);
     } else {
         ofVersion.innerText = 'OpenFin is not available - you are probably running in a browser.';
     }
 });
+
+//Once the DOM has loaded and the OpenFin API is ready
+function onMain() {
+    fin.desktop.System.getVersion(version => {
+	const ofVersion = document.querySelector('#of-version');
+	ofVersion.innerText = version;	
+    });
+}
