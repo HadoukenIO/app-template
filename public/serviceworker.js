e0445903ef8b5ea0a77af9f5a73aabc315c74cc2
Luis Espinola <lespinola@gmail.com>
2018-01-29 10:08:35 -0500

    changed service worker recipe to 'network and cache'

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,30 +1,40 @@
-// Mozilla Service Worker Cookbook 'cache only' recipe: https://serviceworke.rs/strategy-cache-only.html
-var CACHE = "cache-only";
+// Mozilla Service Worker Cookbook 'network or cache' recipe: https://serviceworke.rs/strategy-network-or-cache.html
+var CACHE = "network-or-cache";
 
-self.addEventListener("install", (evt) => {
-    console.log("The service worker is being installed.");
-    evt.waitUntil(precache());
+self.addEventListener("install", function(evt) {
+  console.log("The service worker is being installed.");
+  evt.waitUntil(precache());
 });
 
-self.addEventListener("fetch", (evt) => {
-    console.log("The service worker is serving the asset.");
-    evt.respondWith(fromCache(evt.request));
+self.addEventListener("fetch", function(evt) {
+  console.log("The service worker is serving the asset.");
+  evt.respondWith(
+    fromNetwork(evt.request, 400).catch(function() {
+      return fromCache(evt.request);
+    })
+  );
 });
 
 function precache() {
-    return caches.open(CACHE).then((cache) => {
-        return cache.addAll([
-            "./index.html",
-            "./favicon.ico",
-            "js/main.js"
-        ]);
-    });
+  return caches.open(CACHE).then(function(cache) {
+    return cache.addAll(["./index.html", "./favicon.ico", "js/main.js"]);
+  });
+}
+
+function fromNetwork(request, timeout) {
+  return new Promise(function(fulfill, reject) {
+    var timeoutId = setTimeout(reject, timeout);
+    fetch(request).then(function(response) {
+      clearTimeout(timeoutId);
+      fulfill(response);
+    }, reject);
+  });
 }
 
 function fromCache(request) {
-    return caches.open(CACHE).then((cache) => {
-        return cache.match(request).then((matching) => {
-            return matching || Promise.reject("no-match");
-        });
+  return caches.open(CACHE).then(function(cache) {
+    return cache.match(request).then(function(matching) {
+      return matching || Promise.reject("no-match");
     });
+  });
 }
