4aeb42ceee1728796ccea4e8466b5f1a14b78460
Ricardo de pena <rdepena@Nostromo.local>
2018-08-22 22:28:22 -0400

    using js-adapter, v2API and js modules

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,13 +1,13 @@
 // Mozilla Service Worker Cookbook 'network or cache' recipe: https://serviceworke.rs/strategy-network-or-cache.html
-var CACHE = "network-or-cache";
+const CACHE = 'network-or-cache';
 
-self.addEventListener("install", evt => {
-  console.log("The service worker is being installed.");
+self.addEventListener('install', evt => {
+  console.log('The service worker is being installed.');
   evt.waitUntil(precache());
 });
 
-self.addEventListener("fetch", evt => {
-  console.log("The service worker is serving the asset.");
+self.addEventListener('fetch', evt => {
+  console.log('The service worker is serving the asset.');
   evt.respondWith(
     fromNetwork(evt.request, 400).catch(() => {
       return fromCache(evt.request);
@@ -17,13 +17,13 @@ self.addEventListener("fetch", evt => {
 
 function precache() {
   return caches.open(CACHE).then(cache => {
-    return cache.addAll(["./index.html", "./favicon.ico", "js/main.js"]);
+    return cache.addAll(['./index.html', './favicon.ico', 'js/main.js']);
   });
 }
 
 function fromNetwork(request, timeout) {
   return new Promise((fulfill, reject) => {
-    var timeoutId = setTimeout(reject, timeout);
+    const timeoutId = setTimeout(reject, timeout);
     fetch(request).then(response => {
       clearTimeout(timeoutId);
       fulfill(response);
@@ -34,7 +34,7 @@ function fromNetwork(request, timeout) {
 function fromCache(request) {
   return caches.open(CACHE).then(cache => {
     return cache.match(request).then(matching => {
-      return matching || Promise.reject("no-match");
+      return matching || Promise.reject('no-match');
     });
   });
 }
